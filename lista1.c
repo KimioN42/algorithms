6bb23e7b97e4946339212b059885a54965d81d32
KimioN42 <kimionishino1@gmail.com>
2016-02-23 16:26:08 -0300

    fourth commit, this is almost it

--- lista1.c
+++ lista1.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct NODE{
+	int data;
+	struct NODE *next;
+};
+
+struct MASTER{
+	//tra=0 == queue, tra=1 == stack
+	int size, tra;
+	struct NODE *first, *rear;
+	struct MASTER *next;
+};
+
+typedef struct NODE node;
+typedef struct MASTER master;	
+
+void start (master *head) {
+	head->tra = 0;
+	head->size = 0;
+	head->first = NULL;
+	head->rear = head->first;
+	head->next = NULL;
+}
 
 int main () {
-	printf("hello world\n");
+	master *head;
+
+	start(head);
+	printf("tra: %d\n", head->tra);
+
+
 	return 0;
 }
